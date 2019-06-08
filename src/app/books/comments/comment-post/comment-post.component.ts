72291d4d49709e82cec0a1e528a5efd8428630eb
jguim002 <joaoroasguimaraes@gmail.com>
2019-06-08 18:55:34 -0400

    trying to translate my front end components

--- src/app/books/comments/comment-post/comment-post.component.ts
+++ src/app/books/comments/comment-post/comment-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Comment } from '../comment.model';
 
@@ -12,6 +12,63 @@ export class CommentPostComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @Output() postCreated = new EventEmitter();
+
+  isAuthorized = false;
+  isOwned = true;
+  isLogIn = true;
+  canEdit = false;
+
+  anonymous = false;
+  user = 'jguim002';
+  inputTitle = '';
+  inputData = '';
+
+  onAddPost()
+  {
+    const post =
+    {
+      nickname: this.user,
+      isHidden: this.anonymous,
+      title: this.inputTitle,
+      content: this.inputData
+    };
+    this.canEdit = true;
+    this.postCreated.emit(post);
+  }
+
+  authorize()
+  {
+    if(this.isAuthorized)
+    {
+      this.isAuthorized = false;
+    }else{
+      this.isAuthorized = true;
+    }
+  }
+
+  own()
+  {
+
+    if(this.isOwned)
+    {
+      this.isOwned = false;
+    } else{
+      this.isOwned = true;
+    }
+  }
+
+  logIn()
+  {
+
+    if(this.isLogIn)
+    {
+      this.isLogIn = false;
+    } else{
+      this.isLogIn = true;
+    }
   }
 
 }
