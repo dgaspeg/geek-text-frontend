d6a1bc230c577079065659fc2975bc3b152d947e
jguim002 <joaoroasguimaraes@gmail.com>
2019-06-08 17:57:54 -0400

    added book-detail component and linked my stuff

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { CommentPostComponent } from './comments/comment-post/comment-post.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
-  declarations: [CommentPostComponent],
+  declarations: [CommentPostComponent, BookDetailComponent, CommentListComponent],
   imports: [
     CommonModule,
     BooksRoutingModule
